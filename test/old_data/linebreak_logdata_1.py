0969b0c7a7198b7f58051bda8baa4aa409a2f868
Lilian40629890 <82571839+Lilian40629890@users.noreply.github.com>
2024-12-28 11:40:42 +0800

    Update linebreak_logdata_1.py

--- test/old_data/linebreak_logdata_1.py
+++ test/old_data/linebreak_logdata_1.py
@@ -8,7 +8,7 @@ keywords = ["'timex'", "'temp1'", "'temp2'"]
 input_file = sys.argv[1]  # The first command line parameter is the file path
 
 # Open the original file and read it
-with open(input_file, "r", encoding="utf-8") as file:
+with open(input_file, "r", encoding="latin-1") as file:
     content = file.read()
 
 # Processing content: Line break based on specified keywords
@@ -22,13 +22,13 @@ os.makedirs(output_dir, exist_ok=True)  #Create the folder if it does not exist
 # Save updated content to temporary file
 output_file = "linebreak.txt"
 
-with open(output_file, "w", encoding="utf-8") as temp_file:
+with open(output_file, "w", encoding="latin-1") as temp_file:
     temp_file.write(content)
     
 print(f"File processing completed and saved to {output_file}")
 
 # Read and print the content of the output file for verification
-with open(output_file, "r", encoding="utf-8") as temp_file:
+with open(output_file, "r", encoding="latin-1") as temp_file:
     output_content = temp_file.read()
 
 # Print a portion of the output content to check the result
