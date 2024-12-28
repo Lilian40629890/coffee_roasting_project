b13861a2a3267f5ded933b35cfbc2ab92e5016d3
Lilian40629890 <82571839+Lilian40629890@users.noreply.github.com>
2024-12-28 11:39:29 +0800

    Update linebreak_logdata_1.py

--- src/old_data_management/linebreak_logdata_1.py
+++ src/old_data_management/linebreak_logdata_1.py
@@ -8,7 +8,7 @@ keywords = ["'timex'", "'temp1'", "'temp2'"]
 input_file = sys.argv[1]  # The first command line parameter is the file path
 
 # Open the original file and read it
-with open(input_file, "r", encoding="utf-8") as file:
+with open(input_file, "r", encoding="latin-1") as file:
     content = file.read()
 
 # Processing content: Line break based on specified keywords
@@ -16,13 +16,13 @@ for keyword in keywords:
     content = content.replace(keyword, "\n" + keyword)
 
 # Set the directory where files are stored
-output_dir = "/Users/lilianlee/coffee_database"
+output_dir = "/Users/lilianlee/temporary_files"
 os.makedirs(output_dir, exist_ok=True)  #Create the folder if it does not exist
 
 # Save updated content to temporary file
 output_file = "linebreak.txt"
 
-with open(output_file, "w", encoding="utf-8") as temp_file:
+with open(output_file, "w", encoding="latin-1") as temp_file:
     temp_file.write(content)
 
 print(f"File processing completed and saved to {output_file}")
