c38bbdf25cace8fe6684219800fddb1933b742cd
Lilian40629890 <82571839+Lilian40629890@users.noreply.github.com>
2024-12-28 12:02:55 +0800

    Update delete_temporary_files_4.py

--- src/old_data_management/delete_temporary_files_4.py
+++ src/old_data_management/delete_temporary_files_4.py
@@ -1,7 +1,13 @@
 import os
 
 #Specify the file name to be deleted
-files_to_delete = ["/Users/lilianlee/temporary_files/linebreak.txt", "/Users/lilianlee/temporary_files/timex.txt", "/Users/lilianlee/temporary_files/temp1.txt", "/Users/lilianlee/temporary_files/temp2.txt", "/Users/lilianlee/temporary_files/temp_output.txt", "/Users/lilianlee/temporary_files/timestamp.txt", "/Users/lilianlee/temporary_filesoutput.csv"]
+files_to_delete = [
+    "/Users/lilianlee/temporary_files/linebreak.txt", 
+    "/Users/lilianlee/temporary_files/timex.txt", 
+    "/Users/lilianlee/temporary_files/temp1.txt", 
+    "/Users/lilianlee/temporary_files/temp2.txt", 
+    "/Users/lilianlee/temporary_files/timestamp.txt", 
+    "/Users/lilianlee/temporary_files/output.csv"]
 
 # Delete files
 for file in files_to_delete:
